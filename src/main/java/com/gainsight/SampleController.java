914866926401925d0618ecb6d0d1a7842beaee85
Sridhar Nagaraj <snagaraj@gainsight.com>
2016-07-13 16:35:00 +0530

    pop correcred

--- src/main/java/com/gainsight/SampleController.java
+++ src/main/java/com/gainsight/SampleController.java
@@ -2,11 +2,10 @@ package com.gainsight;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
@@ -21,8 +20,9 @@ import static org.springframework.web.bind.annotation.RequestMethod.GET;
  */
 
 
-@Controller
+
 @EnableAutoConfiguration
+@Controller
 public class SampleController {
 
     private Random random = new Random();
